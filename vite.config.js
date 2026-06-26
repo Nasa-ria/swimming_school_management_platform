cb938311d1f288acd7ce137236eebc6438ed19e4
Nasaria Mumuni <n.mumuni@korba365.com>
2026-06-26 12:08:11 +0000

    fix merge issue

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
->>>>>>> 9c8832ded99e64e1c8985a8b551a90cdbf60e154
 })
